b54470a65b54ac7f2c6a756e10f4de9a7427b99f
sara magdy <Smagdy819@yahoo.com>
2017-03-19 19:24:10 +0200

    about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>about OSC</title>
+   <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/font-awesome.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/normalize.css') }}">
+     <link href="{{ asset('/css/aboutstyle.css') }}" rel="stylesheet">
+    
+</head>
+<body>
+   <div class="container">
+     <div class="row">
+      <img src="{{asset('images/About/about gablia.png')}}" id="about_the_gablia" class="col-s-12"/>
+      <p class="about_text">An inside look from the three founders of CodePen on what it's like running a web business.<br> Everything from server infrastructure, to day-to-day operations, to new feature development.
+     <br> An inside look from the three founders of CodePen on what it's like running a web business. <br>Everything from server infrastructure, to day-to-day operations, to new feature development.
+    <br>  An inside look from the three founders of CodePen on what it's like running a web business.<br> Everything from server infrastructure, to day-to-day operations, to new feature development.</p>
+     <img src="{{asset('images/About/pinguens.png')}}" id="pinguens">
+ </div>
+   </div>
+   <!-- Scripts -->
+    <script src="{{ asset('js/animated-text.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.menu-aim.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/Custom.js') }}"></script>
+   
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+Route::get('/about', function () {
+    return view('about');
+});

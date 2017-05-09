3c2435b781d2d455cec0d6b878b00c4f3db872d5
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-05-09 11:47:34 +0200

    3mlt el task bs feh moshkla fel drop down enma code abdou zy ma hwa el mfrod kolo y3ml push bel gded w y3ml create fel database 3ndo be esm el database ele ana 3amelha fel .env w y3ml php artisan migrate w ele by clone le awl mra y7ot dol el awl composer update php artisan key:generate php artisan config:clear

--- app/Http/Controllers/CommitteesController.php
+++ app/Http/Controllers/CommitteesController.php
@@ -10,14 +10,12 @@ class CommitteesController extends Controller
     public function index1()
     {
         $committees_data = Committee::all();
-
         return view('linux', compact('committees_data'));
     }
 
     public function index2()
     {
         $committees_data = Committee::all();
-
         return view('about', compact('committees_data'));
     }
 }

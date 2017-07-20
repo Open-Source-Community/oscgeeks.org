43cb4e9d67000ec541af0589a8acc37794977b58
sara magdy <Smagdy819@yahoo.com>
2017-07-20 23:07:13 +0200

    fix images at art

--- resources/views/art.blade.php
+++ resources/views/art.blade.php
@@ -29,7 +29,7 @@
 @section('content')
     <h1>ArtWork</h1>
     <p>This is our art team work on our page on facebook or in the events .</p>
-    <img class="img-responsive col-sm-12" src="{{asset('/images/pages images/art/stickers.png')}}">
+    <img class="img-responsive col-sm-12 stickers" src="{{asset('/images/pages images/art/stickers.png')}}">
     <div class="artwork">
         @foreach($committees_data2 as $committee)
             <img tabindex="{{ $committee->id }}" src="{{ $committee->imageurl }}" alt="Image of Ireland">

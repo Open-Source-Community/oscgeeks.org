b515e6ac1b2c772a0b41eb11ba4ba9a89bd95700
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-09-28 01:20:08 +0200

    linux animation

--- resources/views/linux.blade.php
+++ resources/views/linux.blade.php
@@ -14,12 +14,12 @@
     </div>
         <br>
 		<div class="text-center">
-		<p class="hvr-float wow bounceInLeft description" data-wow-duration="3s" data-wow-delay="0.5s" offset="200">
+		<h4 class="hvr-float wow bounceInLeft" data-wow-duration="3s" data-wow-delay="0.5s" offset="200">
 			Have you ever heard the famous saying "windows Linux"?<br>
             Heard it or not, the Linux Moderators Committee is here because of this saying, to show students that there is a whole world beyond windows called Linux.<br>
             Our Committee believes in Open Source knowledge presented in Linux which is our main concern.<br>
             Our Committee believes in spreading knowledge so it provides Linux workshops to students and to our crew, also we are keen to learn and learn so beside our job in spreading knowledge everyday we are learning new Linux stuff.
-		</p>
+		</h4>
 		</div>
         <br>
      <div class="wow bounceInUp" data-wow-duration="3s" data-wow-delay="0.5s" offset="200">

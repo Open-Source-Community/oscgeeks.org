720510c0129ea674abb1b0c3668373cf325d35ef
Mostafa Ahmed <mostafa_4497@hotmail.com>
2017-10-08 14:03:22 +0200

    fix about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,7 +14,7 @@
             <div class="hvr-grow">
             <h1 id="about_the_gablia" class="col-xs-12">About OSC</h1>
             </div>
-            <div class="hvr-float">
+            <div class="hvr-float-shadow">
             <p class="about_text"><span>Open Source Community</span> a student community founded in Faculty of
                 Computer and Information Science - Ain Shams University, on Dec 5th
                 2013.<br> Open Source Community promotes the philosophy of free software
@@ -27,7 +27,7 @@
                 community because we work together in each committee to reach our goal.
                 A person is not just a member in a committee like PR or HR, we work
                 wherever we can because we are OSCians. In other words <span>“ open sourceاحنا بتوع الـ ”</span></p>
-            </div>
+            </div><br><br>
             <img src="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/About/pinguens.png?raw=true" id="pinguens">
         </div>
     </div>

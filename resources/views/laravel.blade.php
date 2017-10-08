5650c1ead6658b85c669784aa3a09b930360aae7
amin <max.mido74@yahoo.com>
2017-10-08 12:11:47 +0200

    fix laravel page

--- resources/views/laravel.blade.php
+++ resources/views/laravel.blade.php
@@ -7,15 +7,15 @@
 @section('content')
 <div class="container ">
     <div class="wow bounceInDown" data-wow-duration="3s" data-wow-delay="0.5s" offset="200">
-    <center><h1 class="hvr-grow-shadow"> Laravel Workshop</h1></center>
+    <center><h1> Laravel Workshop</h1></center>
     </div>
     <div class="wow flipInX" data-wow-duration="3s" data-wow-delay="0.5s" offset="200">
-    <div class="hvr-grow-shadow">
+    <div>
     <h2>Love beautiful code? We do too.</h2>
 
     <p> The world wide web is the present and the future. The whole world is heading towards the cloud and online services everyday; and if you want to be successful in the rapid world of software development then you definitely need to learn how to create beautiful web applications.</p>
     </div>
-    <div class="hvr-grow-shadow">
+    <div>
     <div class="row artwork">
     <img class="img-responsive col-sm-3 " src="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/laravel/logolaravel.jpg?raw=true" alt="Los Angeles">
 
@@ -28,7 +28,7 @@
     </div>
     </div>
     <div class="wow lightSpeedIn" data-wow-duration="3s" data-wow-delay="0.5s" offset="200">
-    <div class="hvr-grow-shadow">
+    <div>
     <h3> MVC Design Pattern</h3>
 
     <p>These developers knew very well how to write code that is clean, beautiful and -most importantly- decoupled. They used a group of really beautiful design patterns, most notably: the MVC design pattern to decouple logic, data access and presentation layers.</p>
@@ -37,7 +37,7 @@
 
     <p>We are the Open Source Community after all right? It is free as in free speech, the code is very well-written and commented, and the documentation is terrific!</p>
     </div>
-    <div class="hvr-grow-shadow">
+    <div>
     <h2>The Laravel Workshop </h2>
 
     <p>Join and enjoy our hands-on project-based workshop. The scope of the workshop is well beyond Laravel; it is also about beautiful code, teamwork, git and other cool stuff.</p>

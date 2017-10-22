9ebc610168b769e94dccbdd5451f699deb2414ef
ahmed tarek <ahmedelmra8y@gmail.com>
2017-10-22 22:03:03 +0200

    make branch

---
+++ resources/views/managment/committees/index.blade.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta property="og:url" content="http://www.oscgeeks.org"/>
+    <meta property="og:image"
+          content="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/navbar/logo-osc.png?raw=true"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to OSC</title>
+    <!-- Title Icon-->
+    <link rel="shortcut icon"
+          href="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/navbar/logo-osc.png?raw=true">
+    <!-- Start Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/font-awesome/css/font-awesome.css">
+    <!--End Fonts -->
+    <!--Start Styles -->
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/Form.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/hover-min.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/layout/header.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/layout/footer.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/layout/nav.css">
+    <link rel="stylesheet" type="text/css" href="http://oscgeeks.org/css/aboutstyle.css">
+
+    <!--End Styles -->
+    <!--Start Scripts -->
+    <script src="http://oscgeeks.org/js/jquery-3.2.1.min.js"></script>
+    <script src="http://oscgeeks.org/js/jquery.menu-aim.js"></script>
+    <script src="http://oscgeeks.org/js/bootstrap.min.js"></script>
+    <script src="http://oscgeeks.org/js/html5shiv.min.js"></script>
+    <script src="http://oscgeeks.org/js/npm.js"></script>
+    <script src="http://oscgeeks.org/js/respond.min.js"></script>
+    <script src="http://oscgeeks.org/js/wow.min.js"></script>
+    <script src="http://oscgeeks.org/js/Custom.js"></script>
+    <script>new WOW().init();</script> <!--End Scripts -->
+    <!--  spiciale   -->
+    <style>
+
+        fieldset.scheduler-border {
+            border: 2px solid #333 !important;
+            padding: 0px 10px 10px 10px !important;
+            border-bottom: none;
+            border-radius: 10px;
+            -webkit-box-shadow: 2px 3px 10px #333;
+            box-shadow: 2px 3px 10px #333;
+            margin: auto !important;
+
+        }
+
+        legend.scheduler-border {
+            font-size: 1.2em !important;
+            font-weight: bold !important;
+            text-align: left !important;
+            width: auto !important;
+            border: none;
+            color: #FFF;
+            font-size: 18px;
+
+        }
+
+        .des h4 {
+            color: #333;
+            font-weight: 800;
+            color: #FFF;
+            margin-bottom: 5px;
+        }
+
+        .des > p {
+            color: #FFF;
+            padding: 20px 30px;
+
+        }
+
+        .dead {
+            float: left;
+            color: #FF651A;
+            font-weight: 800;
+            padding: 15px;
+            background: #333;
+            border-radius: 10px;
+        }
+
+        fieldset button {
+            border: 2px solid #FF651A;
+            color: #FE7701;
+            font-size: 16px;
+            padding: 12px 20px;
+            float: right;
+            border-radius: 10px;
+            background: #333;
+        }
+
+        fieldset button:hover {
+            background: #FF651A;
+            color: #FFF;
+            border: 2px solid #333;
+            cursor: pointer;
+        }
+
+        .col-xs-3 {
+            text-align: center;
+        }
+
+        .col-xs-4 {
+            text-align: center;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        a:hover {
+            color: #fff;
+            text-decoration: none;
+        }
+        .committee:hover{
+            border-radius: 10%;
+            background: #d0a90c;
+        }
+        .footer {
+            position: fixed;
+        }
+    </style>
+</head>
+
+<body>
+<!--  start Navbar  -->
+<div class="navbar navbar-default ">
+    <div class="container-fluid">
+        <!--navbar logos-->
+        <div class="row"><img class=" logo  col-md-1 col-xs-3"
+                              src="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/navbar/logo-osc.png?raw=true">
+            <img class=" osc col-md-4 col-xs-6   "
+                 src="https://raw.githubusercontent.com/Open-Source-Community/oscgeeks.orgImages/master/Minified%20Images/navbar/osc-log.png?raw=true">
+        </div>
+        <!--<div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span
+                        class="sr-only">toggle navigation</span> <span class="icon-bar"></span> <span
+                        class="icon-bar"></span> <span class="icon-bar"></span></button>
+            <!--<a class="navbar-brand" href="a">OSC</a>-->
+        <div class="row">
+            <div class="nav hidden-xs hidden-sm  col-md-12 col-lg-12">
+                <ul class="navigation col-md-9 col-lg-9">
+                    <li><a href="/"><i class="fa fa-home" style="font-size: 20px"></i> <b>Home</b></a></li>
+                    <li>|</li>
+                    <li class="dropdown" style="padding: 0px">
+
+                        <button class="dropbtn"><b>Committees</b> <i class="fa fa-arrow-down"
+                                                                     style="font-size: 9px"></i></button>
+                        <div class="dropdown-content">
+                            <a href="/Linux">Linux Committee</a>
+                            <a href="/Blender">Blender Committee</a>
+                            <a href="/English Heroes">English Heroes Committee</a>
+                            <a href="/Content Creators">Content Creators Committee</a>
+                            <a href="/Artwork">Artwork Committee</a>
+                            <a href="/Projects">Projects Committee</a>
+                            <a href="/Web">Web Committee</a>
+                            <a href="/Public Relations">Public Relations Committee</a>
+                            <a href="/Human Resources">Human Resources Committee</a>
+                            <a href="/Fundraising">Fundraising Committee</a>
+                            <a href="/Logistics">Logistics Committee</a>
+                        </div>
+
+                    </li>
+                    <li>|</li>
+                    <li class="dropdown" style="padding: 0px">
+
+                        <button class="dropbtn"><b>Workshops</b> <i class="fa fa-arrow-down"
+                                                                    style="font-size: 10px"></i></button>
+                        <div class="dropdown-content">
+                            <a href="/Linux Workshop">Linux Workshop</a>
+                            <a href="/Blender Workshop">Blender Workshop</a>
+                            <a href="/Laravel">Laravel Workshop</a>
+                        </div>
+                    </li>
+                    <li>|</li>
+                    <li><a href="/events"><b>Events</b></a></li>
+                    <li>|</li>
+                    <li><a href="/about"><b>About Us</b></a></li>
+                    <!--                    <li>|</li>-->
+                    <!--                    <li><a href="/workShop2017"><b>Workshop Registration</b></a></li>-->
+                </ul>
+
+                <div class="contact_us col-md-3 col-lg-3">
+                    <div class="dropdown">
+                        <button class="dropbtn" onclick="window.location='/Companies'" style="font-size: 12px"><b>Previous
+                            Sponsors</b></button>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn" style="font-size: 12px"><b>Contact Us</b> <i
+                                class="fa fa-arrow-down"></i></button>
+                        <div class="dropdown-content" style="right: -40px">
+                            <a href="/cdn-cgi/l/email-protection#ca9ab8afb9a3aeafa4bee4a5ace48599898aada7aba3a6e4a9a5a7">President
+                                of OSC</a>
+                            <a href="/cdn-cgi/l/email-protection#701f031318020330171d11191c5e131f1d">Hr head of OSC</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- NavBar For Small Screen -->
+
+            <p class="fa fa-bars"></p>
+
+            <div class="menu" style="display: none;margin-top:70px">
+                <div class="nav small_nav hidden-md hidden-lg col-xs-6 col-xs-6">
+
+                    <ul class="navigation">
+                        <li><a href="/"><i class="fa fa-home"></i> <b>Home</b></a></li>
+                        <li class="dropdown">
+
+                            <b>Committees</b> <i class="fa fa-arrow-down" style="font-size: 10px"></i>
+                            <div class="dropdown-content">
+                                <a href="/Linux">Linux Committee</a>
+                                <a href="/Blender">Blender Committee</a>
+                                <a href="/English Heroes">English Heroes Committee</a>
+                                <a href="/Content Creators">Content Creators Committee</a>
+                                <a href="/Artwork">Artwork Committee</a>
+                                <a href="/Projects">Projects Committee</a>
+                                <a href="/Web">Web Committee</a>
+                                <a href="/Public Relations">Public Relations Committee</a>
+                                <a href="/Human Resources">Human Resources Committee</a>
+                                <a href="/Fundraising">Fundraising Committee</a>
+                                <a href="/Logistics">Logistics Committee</a>
+                            </div>
+
+                        </li>
+                        <li class="dropdown" style="padding: 0px">
+
+                            <b>Workshops</b> <i class="fa fa-arrow-down" style="font-size: 10px"></i>
+                            <div class="dropdown-content">
+                                <a href="/Linux Workshop">Linux Workshop</a>
+                                <a href="/Blender Workshop">Blender Workshop</a>
+                                <a href="/Laravel">Laravel Workshop</a>
+                            </div>
+                        </li>
+                        <li><a href="/events"><b>Events</b></a></li>
+                        <li style="padding: 0"><a href="/about"><b>About Us</b></a></li>
+                        <!--                        <li><a href="/workShop2017"><b>Workshop Registration</b></a></li>-->
+                    </ul>
+
+                </div>
+
+                <div class="contact_us hidden-md hidden-lg col-xs-6 col-sm-6">
+                    <div class="dropdown col-xs-12 col-sm-12">
+                        <button class="dropbtn" onclick="window.location='/Companies'"><b>Previous Sponsors</b></button>
+                    </div>
+
+                    <div class="dropdown col-xs-12 col-sm-12">
+                        <button class="dropbtn"><b>Contact Us</b> <i class="fa fa-arrow-down"
+                                                                     style="font-size: 10px"></i></button>
+                        <div class="dropdown-content contact">
+                            <a href="/cdn-cgi/l/email-protection#9bcbe9fee8f2fffef5efb5f4fdb5d4c8d8dbfcf6faf2f7b5f8f4f6">President
+                                of OSC</a>
+                            <a href="/cdn-cgi/l/email-protection#81eef2e2e9f3f2c1e6ece0e8edafe2eeec">Hr head of OSC</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <!-- NavBar For Small Screen -->
+
+    </div>
+</div>
+<div class="row">
+    <div class=" col-xs-push-1 col-xs-10 col-md-6 col-md-push-3 col-lg-6 " style="margin: 30px 0px">
+        <div class="col-xs-3 committee">
+            <a href="#"><h1>Web</h1></a>
+        </div>
+        <div class="col-xs-3 committee">
+            <a href="#"><h1>Web</h1></a>
+        </div>
+        <div class="col-xs-3 committee">
+            <a href="#"><h1>Web</h1></a>
+        </div>
+        <div class="col-xs-3 committee">
+            <a href="#"><h1>Web</h1></a>
+        </div>
+
+        <div class="col-xs-4 committee"><a href="#"><h1>Web</h1></a></div>
+        <div class="col-xs-4 committee"><a href="#"><h1>Web</h1></a></div>
+        <div class="col-xs-4 committee"><a href="#"><h1>Web</h1></a></div>
+
+        <div class="col-xs-3 committee"><a href="#"><h1>Web</h1></a></div>
+        <div class="col-xs-3 committee"><a href="#"><h1>Web</h1></a></div>
+        <div class="col-xs-3 committee"><a href="#"><h1>Web</h1></a></div>
+        <div class="col-xs-3 committee"><a href="#"><h1>Web</h1></a></div>
+
+
+    </div>
+
+</div>
+
+
+<!--start footer-->
+<div class="footer">
+    <div class="row">
+        <center>
+            <div class="col-xs-12">OSC &copy; 2017</div>
+            <br><br>
+
+            <a target="_blank" href="https://www.facebook.com/oscgeeks/" id="" class="splings_link"><i
+                    class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+            <span>&nbsp;&nbsp;</span>
+            <a target="_blank" href="https://twitter.com/oscgeeks" id="" class="splings_link"><i
+                    class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+            <span>&nbsp;&nbsp;</span>
+            <a target="_blank" href="https://www.instagram.com/oscgeeks/" id="" class="splings_link"><i
+                    class="fa fa-instagram fa-3x" aria-hidden="true"></i></a>
+        </center>
+    </div>
+</div>
+<!--End footer-->
+
+
+</body>
+
+</html>

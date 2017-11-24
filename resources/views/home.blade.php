de39c79c7174177ddc5dc10e2e40c022d2e3d134
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2017-11-24 22:45:45 +0200

    test anim

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,20 +31,20 @@
 
 <div class="home" id="home" style="visibility:visible; display:block;">
     <div class="row ">
-        <img class=" osc shapes col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2 " src="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/Background/osc.png?raw=true">
+        {{--<img class=" osc shapes col-lg-6 col-lg-offset-3 col-xs-8 col-xs-offset-2 " src="https://github.com/Open-Source-Community/oscgeeks.orgImages/blob/master/Minified%20Images/Background/osc.png?raw=true">--}}
 
-        <div class="clearfix"></div>
+        {{--<div class="clearfix"></div>--}}
 
-        <div class="message col-lg-3 col-sm-2 col-xs-3 col-xs-offset-7" id="message" >
-            <p>Want to know more ?</p>
-            <p>Let's take a tour.</p>
-        </div>
+        {{--<div class="message col-lg-3 col-sm-2 col-xs-3 col-xs-offset-7" id="message" >--}}
+            {{--<p>Want to know more ?</p>--}}
+            {{--<p>Let's take a tour.</p>--}}
+        {{--</div>--}}
 
-        <div class="clearfix"></div>
+        {{--<div class="clearfix"></div>--}}
 
-        <div class=" rocket col-lg-4 col-lg-offset-4 col-md-5 col-md-offset-3 col-sm-5 col-sm-offset-3 col-xs-5 col-xs-offset-3"></div>
+        {{--<div class=" rocket col-lg-4 col-lg-offset-4 col-md-5 col-md-offset-3 col-sm-5 col-sm-offset-3 col-xs-5 col-xs-offset-3"></div>--}}
 
-        <div class="clearfix"></div>
+        {{--<div class="clearfix"></div>--}}
 
         <a href="{{url("about")}}"  class="button col-lg-2 col-lg-offset-5 col-md-4 col-md-offset-4 col-sm-2 col-sm-offset-4 col-xs-4 col-xs-offset-4">START TOUR</a>
     </div>
